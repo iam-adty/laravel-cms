061c80cb64ce8b19250fff4ee48e70e02700f3a8
adty <iam.adty@gmail.com>
2020-01-29 19:08:44 +0700

    create policies table [close #1]

---
+++ database/migrations/2020_01_29_120737_create_policies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePoliciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('policies', function (Blueprint $table) {
+            $$table->bigIncrements('id');
+            $table->string('code')->nullable(false)->unique();
+            $table->string('name')->nullable(false);
+            $table->text('description')->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('policies');
+    }
+}
